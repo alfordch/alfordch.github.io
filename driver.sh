a4c21027f1927b41f2526163ecdea1458204ecaf
booky <alfordch@umich.edu>
2023-11-25 12:41:56 -0500

    www.livefromthedungeon.net update Sat Nov 25 12:41:56 EST 2023

--- driver.sh
+++ driver.sh
@@ -4,7 +4,7 @@
 # argv[2] : archive #
 # argv[3] : Google Drive main folder ID
 # argv[2:] : recipient_1 recipient_2 ... recipient_n
-python3 playlist_main.py arg1 arg2 arg3 arg4 ...
+python3 playlist_main.py 4 42 1kwPUJgdmGOel56ipOVQtL7MKaeauZRPz charles.alfordiv@gmail.com 
 
 # Run cleanup to get rid of the recording and playlist locally
 bash clean_up
